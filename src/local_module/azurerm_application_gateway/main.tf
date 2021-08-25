8f18851f8ea8f6fed24dfda62f04d1eded31b861
benhpoh <ben.h.poh@gmail.com>
2021-08-25 11:42:25 +1000

    progressing app gateway module

---
+++ src/local_module/azurerm_application_gateway/main.tf
@@ -0,0 +1,81 @@
+resource "azurerm_application_gateway" "network" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  sku {
+    name     = var.sku_name
+    tier     = var.sku_tier
+    capacity = 2
+  }
+
+  dynamic "gateway_ip_configuration" {
+    for_each = var.gateway_ip_configuration
+    content {
+      name      = gateway_ip_configuration.value.name
+      subnet_id = gateway_ip_configuration.value.subnet_id
+    }
+  }
+
+  frontend_port {
+    name = "HTTP"
+    port = 80
+  }
+
+  frontend_port {
+    name = "HTTPS"
+    port = 443
+  }
+
+  dynamic "gateway_ip_configuration" {
+    for_each = var.gateway_ip_configurations
+    content {
+      name      = gateway_ip_configuration.value.name
+      subnet_id = gateway_ip_configuration.value.subnet_id
+    }
+  }
+
+  dynamic "frontend_ip_configuration" {
+    for_each = var.frontend_ip_configurations
+    content {
+      name                         = frontend_ip_configuration.value.name
+      subnet_id                    = try(frontend_ip_configuration.value.subnet_id, null)
+      private_ip_address           = try(frontend_ip_configuration.value.private_ip_address, null)
+      public_ip_address_id         = try(frontend_ip_configuration.value.public_ip_address_id, null)
+      public_ip_address_allocation = try(frontend_ip_configuration.value.public_ip_address_allocation, null)
+    }
+  }
+
+  dynamic "backend_address_pool" {
+    for_each = var.backend_address_pools
+    content {
+      name         = backend_address_pool.value.name
+      fqdns        = try(backend_address_pool.value.fqdns, null)
+      ip_addresses = try(backend_address_pool.value.ip_addresses, null)
+    }
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+}
---
+++ src/local_module/azurerm_application_gateway/variables.tf
@@ -0,0 +1,47 @@
+variable "name" {
+  description = "Application Gateway name"
+}
+variable "resource_group_name" {
+}
+variable "location" {
+}
+
+variable "sku_name" {
+  type        = string
+  description = "The Name of the SKU to use for this Application Gateway. Possible values are Standard_Small, Standard_Medium, Standard_Large, Standard_v2, WAF_Medium, WAF_Large, and WAF_v2."
+
+  validation {
+    condition     = contains(["Standard_Small", "Standard_Medium", "Standard_Large", "Standard_v2", "WAF_Medium", "WAF_Large", "WAF_v2"], var.sku_name)
+    error_message = "Possible values are Standard_Small, Standard_Medium, Standard_Large, Standard_v2, WAF_Medium, WAF_Large, and WAF_v2."
+  }
+}
+
+variable "sku_tier" {
+  type        = string
+  description = "The Tier of the SKU to use for this Application Gateway. Possible values are Standard, Standard_v2, WAF and WAF_v2."
+
+  validation {
+    condition     = contains(["Standard", "Standard_v2", "WAF", "WAF_v2"], var.sku_tier)
+    error_message = "Possible values are Standard, Standard_v2, WAF and WAF_v2."
+  }
+}
+
+variable "sku_capacity" {
+  type        = number
+  description = "When using a V1 SKU this value must be between 1 and 32, and 1 to 125 for a V2 SKU."
+}
+
+variable "gateway_ip_configurations" {
+  type        = list(object({ name : string, subnet_id : string }))
+  description = "A list of objects containing 2 keys: name (Name of this Gateway IP Configuration) & subnet_id (ID of the Subnet which the Application Gateway should be connected to)."
+}
+
+variable "frontend_ip_configurations" {
+  type        = list(any)
+  description = "A list of objects containing the following keys: name, subnet_id, private_ip_address, public_ip_address_id, public_ip_address_allocation"
+}
+
+variable "backend_address_pools" {
+  type        = list(any)
+  description = "A list of objects containing the following keys: name (string), fqdns (list of strings), ip_addresses (list of strings)"
+}
